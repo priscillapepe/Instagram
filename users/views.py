dc84b61ded5de1660e5cf796aacee5be54dfdccd
priscillapepe <priscillaungai99@gmail.com>
2020-10-19 15:09:20 +0300

    modified file

--- users/views.py
+++ users/views.py
@@ -18,7 +18,7 @@ def register(request):
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form':form})
 
-    @login_required
-    def profile(request):
-        return render(request,'users/profile.html')
+@login_required
+def profile(request):
+    return render(request,'users/profile.html')
         
