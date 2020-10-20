f2c4bf982f25cd0ccb1f4bd3d8fe3fc12e2aa982
priscillapepe <priscillaungai99@gmail.com>
2020-10-20 08:53:55 +0300

    modified file

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import profile
 
 
 class UserRegisterForm(UserCreationForm):
@@ -9,3 +10,14 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','email','password','password2']
+
+class UserUpdateForm(forms.ModelForm):
+        model = User
+        fields = ['username','email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        models = profile
+        field = ['image']
+
